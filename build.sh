0da9ac6b669cc154fb2cd80bf7801005a940d17a
Brightside56 <support@4play.by>
2016-08-11 23:59:48 +0300

    Bugfix and nginx update

--- build.sh
+++ build.sh
@@ -2,22 +2,6 @@
 set -e
 
 DOCKER_RUN_IMAGE=nginx-rtmp
-DOCKER_BUILD_IMAGE=nginx-build
-
-#rm -f nginx.tar.gz
-
-cd build
-
-docker build -t ${DOCKER_BUILD_IMAGE} .
-
-cd ..
-
-DID=`docker create ${DOCKER_BUILD_IMAGE}`
-
-docker cp ${DID}:/opt/nginx.tar.gz ./
-
-docker rm ${DID}
-docker rmi ${DOCKER_BUILD_IMAGE}
 
 docker build -t ${DOCKER_RUN_IMAGE} .
 
